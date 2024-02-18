dd957ee4c92ebb9ce612ff1c5e669b8f46e3cb8a
Helaman Bennion <helama22@byu.edu>
2024-02-17 19:02:35 -0700

    add placeholders for js functions

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+/*
+@Params:
+@Returns: void
+Takes login information and checks it for correctness, then calls updateUserName()
+ */
+function submitLogin() {
+    updateUserName();
+}
+
+/*
+@Params: string username
+@Returns: void
+Replaces the login button in top-right corner with username
+ */
+function updateUserName(username) {
+
+}
---
+++ public/js/menu.js
@@ -0,0 +1,27 @@
+/*
+@Params: void
+@Returns: void
+Pulls up order card
+ */
+function makeOrder() {
+
+}
+
+/*
+@Params: void
+@Returns: void
+Processes and uploads order to database, updates order count
+ */
+function submitOrder() {
+
+    updateOrderCount();
+}
+
+/*
+@Params: void
+@Returns: void
+Updates the order count for each donut
+ */
+function updateOrderCount() {
+
+}
