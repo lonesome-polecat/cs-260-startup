9a6b2e40327ee6efff852055242b2d4c6217419a
Helaman Bennion <helama22@byu.edu>
2024-04-03 17:35:46 -0600

    add React, create basic APP with nav layout

---
+++ my-app/src/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Footer() {
+    return(
+        <footer style={{display: "none"}}>
+            <a className="fa fa-facebook" href="https://www.facebook.com/profile.php?id=100091949732954"
+               target="_blank"></a>
+            <a className="fa fa-instagram" href="https://www.instagram.com/arizonuts_utah/" target="_blank"></a>
+        </footer>
+    )
+}
