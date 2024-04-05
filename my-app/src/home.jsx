488b7c9ad441eb115d25d95f84ea021a61f33f8a
Helaman Bennion <helama22@byu.edu>
2024-04-04 21:06:41 -0600

    fix image loading on home page

--- my-app/src/home.jsx
+++ my-app/src/home.jsx
@@ -7,9 +7,9 @@ export function Home() {
         <main style={{display: "flex"}}>
             <p>Welcome to Arizonuts! Begin your tasty adventure...</p>
             <div className="img-row-container">
-                <img className="img-row" alt="donuts1" src="./img/two_donuts_on_platter.jpg"/>
-                <img className="img-row" alt="donuts2" src="img/close_in_basket.jpg"/>
-                <img className="img-row" alt="donuts3" src="img/banana_cream_on_platter.jpg"/>
+                <img className="img-row" alt="donuts1" src={require("./img/two_donuts_on_platter.jpg")} />
+                <img className="img-row" alt="donuts2" src={require("./img/close_in_basket.jpg")} />
+                <img className="img-row" alt="donuts3" src={require("./img/banana_cream_on_platter.jpg")}/>
             </div>
             <a href="https://github.com/lonesome-polecat/startup" target="_blank"><em>GitHub Repo</em></a>
         </main>
