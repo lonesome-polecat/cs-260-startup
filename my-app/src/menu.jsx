22a81afd1fa991273e9f42e5791001cb1b2e0658
Helaman Bennion <helama22@byu.edu>
2024-04-04 21:13:01 -0600

    add basic menu page

--- my-app/src/menu.jsx
+++ my-app/src/menu.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export function Menu() {
    return(
-       <main>
-           <p>This is the menu page</p>
+       <main className="menu">
+           <dialog id="order-dialog">
+           </dialog>
+           <p style={{textAlign: "center"}}>Our weekly pics!</p>
+           <div id="menu-container">
+           </div>
+           <div>
+               <button id="make-order" onClick="makeOrder()">Order Now</button>
+           </div>
        </main>
    )
 }
