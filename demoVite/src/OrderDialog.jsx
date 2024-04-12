369ffd5e212cc49388568d655a8bbb73e4b6c632
Helaman Bennion <helama22@byu.edu>
2024-04-11 22:22:45 -0600

    Enabled input for order amount for each menu item

--- demoVite/src/OrderDialog.jsx
+++ demoVite/src/OrderDialog.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 
 export default function OrderDialog(props) {
+    let [totalCost, setTotalCost] = React.useState(0)
 
-    function eventHandler() {
+    function hideDialog() {
         props.setDialogDisplay(!props.showDialog)
     }
+
 
     return(
         <div>
             {props.showDialog ? (
                 <dialog id="order-dialog" style={{display: 'flex'}}>
-                    <span className={"close-button topright"} onClick={eventHandler}>&times;</span>
+                    <span className={"close-button topright"} onClick={hideDialog}>&times;</span>
                     <p id={"order-dialog-header"}>Complete your order</p>
                     {props.menuItems.map(az => {
                         return(
                             <div className={"order-dialog-option-container"}>
                                 <h3 className={"menu-option-title"} style={az.style}>{az.name}</h3>
-                                {/* This is breaking things*/}
-                                {/*<input id={`${az.id}-order-amount`} className={"amount-input"} type={"number"} defaultValue="0" min="0" step="0" onChange={(e) => {e.preventDefault(); e.target.value += 1}} />*/}
+                                <input id={`${az.id}-order-amount`} className={"amount-input"} type={"number"} defaultValue={0} min={0} step={1} />
                             </div>
                         )
                     })}
-                    <p>Total cost for deliciousness: 0</p>
+                    <p>Total cost for deliciousness: {}</p>
                 </dialog>
             ) : (<div></div>)}
         </div>
