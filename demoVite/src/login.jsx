1ba121ce6fa3ad339271b184596ec4a0c1058ed9
Helaman Bennion <helama22@byu.edu>
2024-04-12 08:12:35 -0600

    Enable switching between login card and createUser card

--- demoVite/src/login.jsx
+++ demoVite/src/login.jsx
@@ -3,13 +3,22 @@ import {LoginCard} from "./LoginCard.jsx";
 import {CreateUserCard} from "./CreateUserCard.jsx";
 
 export function Login() {
+    let [card, setCard] = React.useState('login')
+
+    function changeCard() {
+        setCard(card === 'login' ? 'createUser' : 'login')
+    }
+
     return(
         <main>
             <br />
             <p>Welcome to Arizonuts! Begin your tasty adventure...</p>
             <br />
-            <LoginCard />
-            <CreateUserCard />
+            {card === 'login' ? (
+                <LoginCard changeCard={changeCard}/>
+            ) : (
+                <CreateUserCard changeCard={changeCard}/>
+            )}
             <br />
             <a href="https://github.com/lonesome-polecat/startup" target="_blank"><em>GitHub Repo</em></a>
         </main>
